8c15da2e41f87cceacb03e3d0716718a1d885a46
Micah Raney <MicahRaney@users.noreply.github.com>
2017-07-17 23:55:05 -0700

    Added .gitignore and initial commit

---
+++ src/com/micahraney/abstractsim/Main.java
@@ -0,0 +1,10 @@
+package com.micahraney.abstractsim;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("This is a test");
+        //test
+    }
+}
